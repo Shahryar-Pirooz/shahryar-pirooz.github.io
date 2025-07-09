6cabd3181878fe3b8058458cc5435c07b1e17580
Shahryar Pi <dev.shahryar@gmail.com>
2025-07-09 21:32:25 +0330

    Enhance styling and structure in globals.css and banner.tsx
    - Added selection styling in globals.css for better user experience.
    - Updated banner.tsx to include marker styles for highlighted text.

--- app/sections/banner.tsx
+++ app/sections/banner.tsx
@@ -1,12 +1,14 @@
 import BlueBtn from '@/components/blue_btn/blue_btn'
 export default function Banner() {
+	const marker =
+		'before:content-[" "] before:bg-[#0b6efd87] before:w-[102%] before:h-5 before:-z-3 before: before:bottom-1 before:absolute relative '
 	return (
 		<section className='flex flex-col justify-center text-center w-full h-full min-h-screen space-y-5'>
 			<div className='text-2xl font-bold'>meet Shahryar Pirooz</div>
 			<div className='text-7xl font-bold leading-tight'>
-				Open source soul. <wbr />
+				<span className={`${marker}`}>Open source</span> soul. <wbr />
 				Backend bones. <wbr />
-				Systems built to last.
+				Systems <span className={`${marker}`}>built to last</span>.
 			</div>
 			<div className='text-base'>
 				I build systems that serve people—not just specs.
