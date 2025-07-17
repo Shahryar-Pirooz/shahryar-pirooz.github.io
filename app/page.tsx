1432a8384221c48613cacc5b9b081e6b25f7b6a5
Shahryar Pi <dev.shahryar@gmail.com>
2025-07-17 13:43:43 +0330

    Add Contact section to Home component for improved site navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import AboutMe from './sections/about'
 import Banner from './sections/banner'
+import Contact from './sections/contact'
 import Projects from './sections/projects'
 export default function Home() {
 	return (
-		<div className='container mx-auto flex flex-col w-screen px-4'>
+		<div className='flex flex-col w-screen px-4'>
 			<Banner />
 			<Projects />
 			<AboutMe />
+			<Contact />
 		</div>
 	)
 }
