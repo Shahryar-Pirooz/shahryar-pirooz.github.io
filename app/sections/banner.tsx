773a336332cdcaedb3a6ef15a2e336b4d3f736ce
Shahryar Pi <dev.shahryar@gmail.com>
2025-07-17 13:42:57 +0330

    Refactor Banner section: update text content for clarity and impact

--- app/sections/banner.tsx
+++ app/sections/banner.tsx
@@ -3,15 +3,18 @@ export default function Banner() {
 	const marker =
 		'before:content-[" "] before:bg-[#0b6efd87] before:w-[102%] before:h-5 before:-z-3 before: before:bottom-1 before:absolute relative '
 	return (
-		<section className='flex flex-col justify-center text-center w-full h-full min-h-screen space-y-5 px-8'>
+		<section
+			id='home'
+			className='flex flex-col justify-center text-center w-full h-full min-h-screen space-y-5 px-8'
+		>
 			<h1 className='text-2xl font-bold'>meet Shahryar Pirooz</h1>
 			<div className='text-7xl font-bold leading-tight'>
-				<span className={`${marker}`}>Open source</span> soul. <wbr />
-				Backend bones. <wbr />
-				Systems <span className={`${marker}`}>built to last</span>.
+				<span className={`${marker}`}>Code with purpose</span>. <wbr />
+				Architect at heart. <wbr />
+				Systems <span className={`${marker}`}>crafted to scale</span>.
 			</div>
 			<div className='text-base'>
-				I build systems that serve people—not just specs.
+				I don’t just code — I design, debug, and deliver.
 			</div>
 			<div className='justify-center items-center mx-auto'>
 				<BlueBtn />
