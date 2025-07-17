ba9524a2640c0307e229362034174b36adc4fa3d
Shahryar Pirooz <dev.Shahryar@gmail.com>
2025-07-17 14:20:00 +0330

    Update footer.tsx

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
 				<div className='text-center md:text-left space-y-2'>
 					<p>Shahryar Pirooz © {new Date().getFullYear()}</p>
 					<p className='text-gray-400'>
-						Based in Ireland • dev.shahryar@gmail.com
+						Based in Iran • dev.shahryar@gmail.com
 					</p>
 				</div>
 
@@ -15,7 +15,7 @@ export default function Footer() {
 					<p>
 						Design inspired by{' '}
 						<Link
-							href='https://github.com/morganjlopes/'
+							href='https://github.com/morganjlopes/portfolio-template'
 							target='_blank'
 							className='underline underline-offset-2 text-accent hover:text-accent/80 transition'
 						>
@@ -25,7 +25,7 @@ export default function Footer() {
 					<p className='text-gray-500'>
 						Source available on{' '}
 						<Link
-							href='https://github.com/Shahryar-Pirooz'
+							href='https://github.com/Shahryar-Pirooz/shahryar-pirooz.github.io'
 							target='_blank'
 							className='text-accent hover:text-accent/80 underline underline-offset-2'
 						>
